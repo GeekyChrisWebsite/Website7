590e517e1e148e22460f04a1d52e4ede5a65ad6a
rania-ibrhem30 <raniamansour967@gmail.com>
2023-12-15 15:17:18 +0200

    map

--- src/app/environments/environment.development.ts
+++ src/app/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     BACKEND_DOMAIN: 'http://34.236.124.150:5000',
     SITE_DOMAIN: 'post-your-biz7.vercel.app',
-
+    map_apikey:'AIzaSyAAb4SFQe0Kigpu3EiKhR93nC-OUitTKwM'
 };
--- src/app/listing/listing/listing.component.ts
+++ src/app/listing/listing/listing.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import {  Component, ViewEncapsulation,AfterViewInit, ViewChild, ElementRef, AfterViewChecked } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DropdownModule } from 'primeng/dropdown';
 import { ButtonModule } from 'primeng/button';
@@ -6,18 +6,21 @@ import { CardModule } from 'primeng/card';
 import { ListingService } from '../listing.service';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { DetalisListingComponent } from '../detalis-listing/detalis-listing.component';
+import { GoogleMapsModule } from '@angular/google-maps';
+import { Observable, catchError, map, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-listing',
   standalone: true,
-  imports: [CommonModule,DropdownModule,ButtonModule,CardModule,RouterLink,DetalisListingComponent],
+  imports: [CommonModule, DropdownModule, ButtonModule, CardModule, RouterLink, DetalisListingComponent, GoogleMapsModule],
   templateUrl: './listing.component.html',
   styleUrl: './listing.component.scss',
-  encapsulation:ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None
 
 })
-export class ListingComponent {
-  data:any;
+export class ListingComponent  implements AfterViewChecked{
+  data: any;
   fliterarray: any[] = [];
   searcharray: any[] = [];
   cities: any[] | undefined;
@@ -35,26 +38,55 @@ export class ListingComponent {
   rows = 2;
   searchQuery: string = '';
   arrayData: any;
-  loading:boolean = false
-
-
-
-  constructor(public service:ListingService , public _router:ActivatedRoute) {
+  loading: boolean = false
+  apiLoaded?: Observable<boolean>;
+  options:any = {
+    mapTypeControl: false,
+    streetViewControl: false,
+    fullscreenControl: false,
   }
-  getlisting() {
-    this.loading= true
-    this.service.GetListing().subscribe((res: any) => {
-      this.loading= false
-      this.data = res
-      console.log(this.data)
+  isApiLoaded: boolean = false; // Assuming you have a mechanism to check API loading status
 
-    })
+  @ViewChild('map') mapElement: ElementRef | undefined;
+
+
+
+  constructor(public service: ListingService, public _router: ActivatedRoute, private httpClient: HttpClient) {
+
+    this.apiLoaded = httpClient.jsonp('https://maps.googleapis.com/maps/api/js?key=AIzaSyAAb4SFQe0Kigpu3EiKhR93nC-OUitTKwM', 'callback')
+      .pipe(
+        map(() => true),
+        catchError(() => of(false)),
+      );
+  }
+  ngAfterViewChecked(): void {
+    throw new Error('Method not implemented.');
+  }
+  
+  
+  // getlisting() {
+  //   this.loading = true
+  //   this.service.GetListing().subscribe((res: any) => {
+  //     this.loading = false
+  //     this.data = res
+  //     console.log(this.data)
+
+  //   })
+  // }
+  getlisting() {
+    this.loading = true;
+    this.service.GetListing().subscribe((res: any) => {
+      this.loading = false;
+      this.data = res;
+      console.log(this.data);
+  
+    });
   }
 
   fliterlistingBIZ() {
     this.loading = true
     this.service.fliterListing().subscribe((res: any) => {
-      this.fliterarray = res.body; 
+      this.fliterarray = res.body;
       this.data = this.fliterarray;
       this.loader = false
       console.log(this.fliterarray)
@@ -71,7 +103,7 @@ export class ListingComponent {
     if (this.searchQuery.trim() === '') {
       this.searcharray = this.data;
     } else {
-      this.searcharray = this.data.filter((item:any) =>
+      this.searcharray = this.data.filter((item: any) =>
         item.name.toLowerCase().includes(this.searchQuery.toLowerCase())
       );
     }
@@ -138,20 +170,22 @@ export class ListingComponent {
     ];
 
     ;
-   ;
-   this._router.queryParams.subscribe(params => {
+    ;
+    this._router.queryParams.subscribe(params => {
       // console.log(params)
-      if(Object.keys(params).length) {
+      if (Object.keys(params).length) {
         this.service.fliterListing(params).subscribe(res => {
-          this.data=res.body
+          this.data = res.body
           console.log(this.data)
         })
       } else {
         this.getlisting()
       }
 
-   })
+    })
   }
- 
+  
 
+
+  
 }
