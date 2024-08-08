3e7aaf52d77ebc43700bab5a253bfc03f028fe4d
rania-ibrhem30 <raniamansour967@gmail.com>
2024-08-08 13:30:09 +0300

    edit similar

--- src/app/interface/buysell-detalis.ts
+++ src/app/interface/buysell-detalis.ts
@@ -1,58 +1,56 @@
 export interface BuysellDetalis {
-
-    author: string;
-    bold: boolean;
-    business: business
-    business_id: number;
-    buySellTime: string;
-    content: string;
-    created_at: string;
-    dates: null;
-    days_until_expiration: null;
-    expire_at: null;
-    expired: boolean;
-    expired_soon: boolean;
-    hide: boolean;
-    id: number;
-    images: any;
-    integrations: any[];
-    italic: boolean;
-    likes: number;
-    price: number;
-    recurring_every: null;
-    recurring_for: null;
-    recurring_on: null;
-    sold: boolean;
-    status: string;
-    time: null;
-    title: string;
-    type: string;
-    u_end_date: null;
-    u_scheduled_date: null;
-    u_selected_days: null;
-    u_start_date: null;
-    updated_at: string;
-    video: string;
-    vip: boolean;
-    websites: null;
-    state: string;
-    street: string;
-    phone_number: string;
-    booking_url: string;
-    geo_direction: {
-        lat: number;
-        lng: number;
-    },
-    logo: string,
-    name: string
-
-
+  author: string;
+  bold: boolean;
+  business: business;
+  business_id: number;
+  buySellTime: string;
+  content: string;
+  created_at: string;
+  dates: null;
+  days_until_expiration: null;
+  expire_at: null;
+  expired: boolean;
+  expired_soon: boolean;
+  hide: boolean;
+  id: number;
+  images: any;
+  integrations: any[];
+  italic: boolean;
+  likes: number;
+  price: number;
+  recurring_every: null;
+  recurring_for: null;
+  recurring_on: null;
+  sold: boolean;
+  status: string;
+  time: null;
+  title: string;
+  type: string;
+  u_end_date: null;
+  u_scheduled_date: null;
+  u_selected_days: null;
+  u_start_date: null;
+  updated_at: string;
+  video: string;
+  vip: boolean;
+  websites: null;
+  state: string;
+  street: string;
+  phone_number: string;
+  booking_url: string;
+  geo_direction: {
+    lat: number;
+    lng: number;
+  };
+  logo: string;
+  name: string;
 }
 export interface business {
-    geo_direction: {
-        lat: number;
-        lng: number;
-    },
-    logo: string,
-    name: string
+  geo_direction: {
+    lat: number;
+    lng: number;
+  };
+  logo: string;
+  name: string;
+  booking_url: string;
 }
